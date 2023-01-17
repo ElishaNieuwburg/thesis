scaling fixed, changed to json file

WTDataset.py
@@ -1,4 +1,5 @@
 # Class for loading the data for Pytorch models
+# TODO: change to JSON file input
 import os
 import torch
 import numpy as np
@@ -56,7 +57,7 @@ class WTDataset(torch.utils.data.Dataset):
 
         return data_list
 
-    def __getitem__(self, idx: int) -> tuple:
+    def __getitem__(self, idx: int) -> tuple(Image, dict):
         data_pair = self.data_list[idx]
         img = Image.open(os.path.join(self.imgs, data_pair['img_name']))
         target = data_pair['target']

augmentation.py
@@ -1,75 +1,74 @@
-import math
 import random
 import numpy as np
 from PIL import Image
-from torchvision import transforms as T
 from torchvision.transforms import functional as F
 
 
-class Augmentation():
+# Flip the image and boxes horizontally
+def flip_hor(image: Image, boxes: np.ndarray) -> tuple(Image, np.array):
+    image = F.hflip(image)
+    copied_boxes = np.copy(boxes)
+    copied_boxes[:, [0, 2]] = 1 - copied_boxes[:, [0, 2]]
+    return image, copied_boxes
 
-    def __init__(self):
-        self.chance = 0.5
-        self.saturation = random.uniform(1, 3)
-        self.contrast = random.uniform(1, 3)
-        self.gamma = random.uniform(0.3, 2)
-        self.hue = random.uniform(-0.5, 0.5)
-        self.kernel = random.randrange(1, 20, 2)
-        self.scale = 1.5
-        # print(random.uniform(0.5, 1.5))
 
-    def flip_hor(self, image, boxes):
-        image = F.hflip(image)
-        boxes[:, [0, 2]] = 1 - boxes[:, [0, 2]]
-        return image, boxes
+# Scale image and bounding boxes to certain scale
+def scale_img(image: Image, boxes: np.ndarray, labels: np.ndarray, scale: float, alpha=1e-1) -> tuple(Image, np.ndarray, np.ndarray):
+    # Resize image
+    original_size = F.get_image_size(image)
+    width = original_size[0] / scale
+    height = original_size[1] / scale
+    image = F.resized_crop( image,
+                            (original_size[1] - height) / 2,
+                            (original_size[0] - width) / 2,
+                            height,
+                            width,
+                            [original_size[1], original_size[0]])
+    
+    # Resize the boxes according to scale
+    copied_boxes = scale * (np.copy(boxes) - 0.5) + 0.5
 
-    def saturate(self, image):
-        image = F.adjust_brightness(image, self.saturation)
-        return image
-        
-    def change_contrast(self, image):
-        image = F.adjust_contrast(image, self.contrast)
-        return image
+    # Delete boxes with an area close to zero if zoomed in
+    if scale > 1:
+        clipped_boxes = np.copy(copied_boxes)
+        clipped_boxes[clipped_boxes < 0] = 0
+        clipped_boxes[clipped_boxes > 1] = 1
 
-    def change_gamma(self, image):
-        image = F.adjust_gamma(image, self.gamma)
-        return image
+        areas = (clipped_boxes[:, 3] - clipped_boxes[:, 1]) * (clipped_boxes[:, 2] - clipped_boxes[:, 0])
+        indexes = []
+        for i, area in enumerate(areas):
+            if np.isclose(area, 0, atol=alpha):
+                indexes.append(i)
+            
+        copied_boxes = np.delete(copied_boxes, indexes, axis=0)
+        copied_labels = np.delete(np.copy(labels), indexes, axis=0)
 
-    def change_hue(self, image):
-        image = F.adjust_hue(image, self.hue)
-        return image
+        return image, copied_boxes, copied_labels
+    
+    return image, copied_boxes, labels
 
-    def blur(self, image):
-        image = F.gaussian_blur(image, self.kernel)
-        return image
 
-    def scale_img(self, image, boxes):
-        # TODO: Fix deleting bounding boxes and labels that have no area
-        original_size = F.get_image_size(image)
-        width = original_size[0] / self.scale
-        height = original_size[1] / self.scale
-        top = (original_size[1] - height) / 2
-        left = (original_size[0] - width) / 2
-        image = F.resized_crop(image, top, left, height, width, [original_size[1], original_size[0]])
-        print("old boxes", boxes)
-        boxes = self.scale * (boxes - 0.5) + 0.5
-        boxes[boxes < 0] = 0
-        boxes[boxes > 1] = 1
-        print("middle boxes", boxes)
-        areas = (boxes[:, 3] - boxes[:, 1]) * (boxes[:, 2] - boxes[:, 0])
+# Augment the image using multiple augmentation techniques
+def augment(image_path: str, boxes: np.ndarray, labels: np.ndarray, flip_chance=0.5, scale_chance=0.5) -> tuple(Image, np.ndarray, np.ndarray):
+    image = Image.open(image_path)
 
-        # mask = (z[:, 0] == 6)
-        # z[mask, :]
+    # Flip horizontally
+    if random.uniform(0, 1) > flip_chance:
+        image, boxes = flip_hor(image, boxes)
 
-        # final_boxes = np.empty((0,4), float)
-        # for i, area in enumerate(areas):
-        #     if not math.isclose(area, 0, rel_tol=1e-5):
-        #         final_boxes = np.append(final_boxes, np.array([boxes[i]]), axis=0)
+    # Change scale of image
+    if random.uniform(0, 1) > scale_chance:
+        image, boxes, labels = scale_img(image, boxes, labels, scale=random.uniform(0.5, 1.5))
+    
+    # Adjust HSV and blur
+    image = F.adjust_brightness(image, random.uniform(1, 2))
+    image = F.adjust_contrast(image, random.uniform(1, 2))
+    image = F.adjust_gamma(image, random.uniform(0.5, 1))
+    image = F.adjust_hue(image, random.uniform(-0.5, 0.5))
+    image = F.gaussian_blur(image, random.randrange(1, 20, 2))
 
-        print(areas)
-        print("final boxes", boxes)
+    return image, boxes, labels
+    
 
-        return image, boxes
 
-    # def augment(self, data):
-    #     if random.uniform(0, 1) > self.chance:
+

helper.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 # Find the corner points for drawing the boxes
-def draw(box, img_shape):
+def draw(box: np.ndarray, img_shape: list) -> float:
     x0  = (box[0] - np.abs(box[0] - box[2]) / 2.) * img_shape[0]
     x1 = (box[0] + np.abs(box[0] - box[2]) / 2.) * img_shape[0]
     y0   = (box[1] - np.abs(box[1] - box[3]) / 2.) * img_shape[1]